c7e2302083f024a4b528d57e022ca644f14a81d0
kaushik Bansode <83575405+KB-2000@users.noreply.github.com>
2024-02-22 13:00:12 +0530

    Add files via upload

---
+++ delete_page.php
@@ -0,0 +1,14 @@
+<?php include("dbconn.php"); ?>
+
+<?php
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $query = "delete from `students` where `id` = '$id'";
+        $result = mysqli_query($connection, $query);
+        if(!$result){
+            die("Query failed".mysqli_error($connection));
+        }else{
+            header("location:index.php?delete_msg=You have successfully deleted record");
+        }
+    }
+?>
---
+++ header.php
@@ -0,0 +1,14 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></head>
+    <link rel="stylesheet" href="style.css">
+    <body>
+    <h1 id="main_title">CRUD APPLICATION IN PHP</h1>
+    <div class="container">
+
